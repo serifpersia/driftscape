0d29bfd49880f72fba4ecbe3df8702447ebbc1b9
serifpersia <ramiserifpersia@gmail.com>
2024-04-27 19:37:07 +0200

    laser line2d improvements

--- scripts/laser.gd
+++ scripts/laser.gd
@@ -1,6 +1,6 @@
 extends RayCast2D
 
-@export var MAX_LENGTH = 2000
+const MAX_LENGTH = 2000
 
 @onready var laser_raycast = $"."
 @onready var line_2d = $Line2D
@@ -13,14 +13,20 @@ func _physics_process(_delta):
 	laser_raycast.target_position = Vector2(0, MAX_LENGTH)
 	
 	var collision_point = laser_raycast.get_collision_point()
+	var local_collision_point = line_2d.to_local(collision_point)
 	
 	if laser_raycast.is_colliding():
-		var local_collision_point = line_2d.to_local(collision_point)
-		line_2d.points[1] = local_collision_point
-		
 		var collider = laser_raycast.get_collider()
+		
 		if collider is Player:
-			player =  collider
+			line_2d.points[1] = local_collision_point
+			player = collider
 			player.decrease_health(2)
+		else:
+			var normal = laser_raycast.get_collision_normal()
+			var line_width = line_2d.get("width")
+			local_collision_point -= normal * (line_width / 2.0)
+			line_2d.points[1] = local_collision_point
+	
 	if !laser_sound.playing:
 		laser_sound.play()
